fda51f1b1f27cd4d470ae3109ddc961b9882bb4b
Conor Linehan <conorlinehan9@gmail.com>
2016-10-04 19:46:02 +0100

    sets up start editor controller

--- app/controllers/editor.js
+++ app/controllers/editor.js
@@ -5,19 +5,4 @@ const DEBOUNCE_TIME = (Ember.testing) ? 20 : 500;
 const FIVE_SECONDS = (Ember.testing) ? 40 : 5000;
 
 export default Ember.Controller.extend({
-  saveModelTask: task(function *() {
-    yield this.get('model').save();
-  }).keepLatest(),
-
-  forceSaveTask: task(function *() {
-    yield timeout(FIVE_SECONDS);
-    this.get('saveModelTask').perform();
-  }).drop(),
-
-  updatedModelTask: task(function *() {
-    this.get('forceSaveTask').perform();
-    yield timeout(DEBOUNCE_TIME);
-    this.get('forceSaveTask').cancelAll();
-    this.get('saveModelTask').perform();
-  }).restartable()
 });
