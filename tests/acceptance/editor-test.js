7f8323b5c0e45fcb4adf444212ba44d064f7928f
Conor Linehan <conorlinehan9@gmail.com>
2016-10-04 20:02:41 +0100

    starting test

--- tests/acceptance/editor-test.js
+++ tests/acceptance/editor-test.js
@@ -11,55 +11,16 @@ const triggerInput = (editor, text, $element) =>{
   }, $element);
 };
 
-moduleForAcceptance('Acceptance | editor');
+moduleForAcceptance('Acceptance | editor', {
+  beforeEach() {
+    server.create('post', {text: ''});
+  }
+});
 
 test('it debounces a save', function(assert) {
-  server.create('post', {text: ''});
-
-  visit('/');
-  andThen(() =>{
-    // Grab MediumEditor Instance
-    let $editor = Ember.$('.editor')[0];
-    let editor = MediumEditor.getEditorFromElement($editor);
-
-    triggerInput(editor, 'old Text', $editor);
-    timeout(5)
-    .then(() =>{
-      triggerInput(editor, 'new text', $editor);
-      timeout(25)
-      .then(() =>{
-        assert.equal(server.db.posts[0].text, 'new text');
-        let numberOfSaves = server.pretender.handledRequests
-        .filter(r => r.method === 'PATCH').length;
-        assert.equal(numberOfSaves, 1);
-      });
-    });
-  });
+  // [Enter Code Here]
 });
 
 test('it forces a save', function(assert) {
-  server.create('post', { text: '' });
-
-  // Same as above
-  visit('/');
-  andThen(() =>{
-
-    let $editor = Ember.$('.editor')[0];
-    let editor = MediumEditor.getEditorFromElement($editor);
-
-    // 1
-    triggerInput(editor, '1', $editor);
-    timeout(15).then(() =>{
-      triggerInput(editor, '2', $editor);
-      timeout(15).then(() =>{
-        triggerInput(editor, '3', $editor);
-        timeout(15).then(() =>{
-          // 2
-          assert.equal(server.db.posts[0].text, '3');
-          assert.equal(server.pretender.handledRequests.length, 2);
-        });
-      });
-    });
-
-  });
+  // [Enter Code Here]
 });
